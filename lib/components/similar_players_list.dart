65c60e2958ec45510a654e3ecfff7ec86c314889
Felipe Araujo <fbma@cin.ufpe.br>
2023-10-06 19:23:34 -0300

    feat(TeamsViewScreen): Implement Similar Players List widget

---
+++ lib/components/similar_players_list.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:voleizinho/model/player.dart';
+import 'package:voleizinho/services/team_match_service.dart';
+
+class SimilarPlayersList extends StatelessWidget {
+  const SimilarPlayersList(
+      {super.key, required this.player, required this.onPlayerSwitch});
+
+  final Player player;
+  final void Function(Player) onPlayerSwitch;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        for (Player similarPlayer
+            in TeamMatchService.getSimilarPlayers(player).take(3))
+          Material(
+            elevation: 3,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      similarPlayer.name!,
+                      style: const TextStyle(
+                        fontFamily: "poller_one",
+                        color: Colors.blue,
+                        fontSize: 10,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Similaridade: ${(100 * player.similarity(similarPlayer)).toStringAsFixed(0)}%",
+                          style: const TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "Média: ${TeamMatchService.avgDiffOnSwap(player, similarPlayer) > 0 ? "+" : ""}${TeamMatchService.avgDiffOnSwap(player, similarPlayer).toStringAsFixed(2)}",
+                          style: const TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        GestureDetector(
+                          onTap: () => onPlayerSwitch(similarPlayer),
+                          child: const Icon(Icons.change_circle),
+                        ),
+                      ],
+                    )
+                  ]),
+            ),
+          )
+      ],
+    );
+  }
+}
--- lib/screens/teams_view_screen.dart
+++ lib/screens/teams_view_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:voleizinho/components/player_card.dart';
+import 'package:voleizinho/components/similar_players_list.dart';
 import 'package:voleizinho/model/player.dart';
 import 'package:voleizinho/model/team.dart';
 import 'package:voleizinho/services/team_match_service.dart';
@@ -83,69 +84,16 @@ class _TeamsViewScreenState extends State<TeamsViewScreen> {
                           ),
                         ),
                         if (switchingPlayer == player)
-                          Column(
-                            children: [
-                              for (Player similarPlayer
-                                  in TeamMatchService.getSimilarPlayers(player)
-                                      .take(3))
-                                Material(
-                                  elevation: 3,
-                                  child: Container(
-                                    width: double.infinity,
-                                    padding: const EdgeInsets.all(8),
-                                    child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            similarPlayer.name!,
-                                            style: const TextStyle(
-                                              fontFamily: "poller_one",
-                                              color: Colors.blue,
-                                              fontSize: 10,
-                                            ),
-                                          ),
-                                          Row(
-                                            children: [
-                                              Text(
-                                                "Similaridade: ${(100 * player.similarity(similarPlayer)).toStringAsFixed(0)}%",
-                                                style: const TextStyle(
-                                                    fontSize: 10,
-                                                    fontWeight: FontWeight.bold,
-                                                    color: Colors.blue),
-                                              ),
-                                              const SizedBox(
-                                                width: 20,
-                                              ),
-                                              Text(
-                                                "Média: ${TeamMatchService.avgDiffOnSwap(player, similarPlayer) > 0 ? "+" : ""}${TeamMatchService.avgDiffOnSwap(player, similarPlayer).toStringAsFixed(2)}",
-                                                style: const TextStyle(
-                                                    fontSize: 10,
-                                                    fontWeight: FontWeight.bold,
-                                                    color: Colors.blue),
-                                              ),
-                                              const SizedBox(
-                                                width: 20,
-                                              ),
-                                              GestureDetector(
-                                                onTap: () => {
-                                                  TeamMatchService.swapPlayers(
-                                                      player, similarPlayer),
-                                                  setState(() {
-                                                    switchingPlayer = null;
-                                                    teams =
-                                                        TeamMatchService.teams;
-                                                  })
-                                                },
-                                                child: const Icon(
-                                                    Icons.change_circle),
-                                              ),
-                                            ],
-                                          )
-                                        ]),
-                                  ),
-                                )
-                            ],
+                          SimilarPlayersList(
+                            player: player,
+                            onPlayerSwitch: (Player similarPlayer) => {
+                              TeamMatchService.swapPlayers(
+                                  player, similarPlayer),
+                              setState(() {
+                                switchingPlayer = null;
+                                teams = TeamMatchService.teams;
+                              })
+                            },
                           ),
                       ],
                     )
