5413faf13be4fef42a636ac8dce67ac842a7bccb
Felipe Araujo <fbma@cin.ufpe.br>
2023-11-15 18:20:08 -0300

    refactor(SkillGauge): export component

--- lib/screens/group/group_creation/group_creation_screen.dart
+++ lib/screens/group/group_creation/group_creation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:voleizinho/model/group.dart';
 import 'package:voleizinho/model/skills.dart';
+import 'package:voleizinho/screens/settings_screen.dart';
 import 'package:voleizinho/services/group_service.dart';
 
 class GroupCreationScreen extends StatefulWidget {
@@ -165,42 +166,6 @@ class _GroupCreationScreenState extends State<GroupCreationScreen> {
           ),
         ),
       ),
-    );
-  }
-}
-
-class SkillGauge extends StatelessWidget {
-  const SkillGauge(
-      {super.key,
-      required this.value,
-      required this.onChanged,
-      required this.label});
-
-  final int value;
-  final int maxValue = 10;
-  final String label;
-  final void Function(double) onChanged;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Expanded(
-          child: Text(label,
-              style: const TextStyle(fontSize: 10, fontFamily: "poller_one")),
-        ),
-        Expanded(
-          child: Slider(
-            value: value.toDouble(),
-            min: 0,
-            max: 10,
-            divisions: 10,
-            label: value.toString(),
-            onChanged: onChanged,
-          ),
-        ),
-      ],
     );
   }
 }
