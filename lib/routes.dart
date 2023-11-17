dabde0b342e68f83d56ecfd9109a6bd6d9ee56ec
Felipe Araujo <fbma@cin.ufpe.br>
2023-11-17 01:25:09 -0300

    feat(Settings Screen): Use GroupBloc to update and delete a group

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
 import 'package:voleizinho/screens/group/group_home/group_home_screen.dart';
 import 'package:voleizinho/screens//group/group_creation/group_creation_screen.dart';
-import 'package:voleizinho/screens/group_settings/settings_screen.dart';
+import 'package:voleizinho/screens/group/group_settings/settings_screen.dart';
 import 'package:voleizinho/screens/home/home_screen.dart';
 import 'package:voleizinho/screens/players/players_screen.dart';
 import 'package:voleizinho/screens/scoreboard_screen.dart';
@@ -8,7 +8,7 @@ import 'package:voleizinho/screens/team_creation_screen.dart';
 import 'package:voleizinho/screens/teams_view_screen.dart';
 
 var routes = {
-  "/": (context) => HomeScreen(),
+  "/": (context) => const HomeScreen(),
   "/group_creation": (context) => const GroupCreationScreen(),
   "/main_group": (context) => const GroupHomeScreen(),
   "/players": (context) => const PlayersScreen(),
--- lib/screens/group/group_settings/settings_screen.dart
+++ lib/screens/group/group_settings/settings_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:voleizinho/bloc/groups/group_bloc.dart';
+import 'package:voleizinho/bloc/groups/group_events.dart';
+import 'package:voleizinho/bloc/groups/group_states.dart';
 import 'package:voleizinho/components/drawer.dart';
-import 'package:voleizinho/components/skill_gauge.dart';
+import 'package:voleizinho/screens/group/components/skill_gauge.dart';
 import 'package:voleizinho/model/group.dart';
 import 'package:voleizinho/model/skills.dart';
-import 'package:voleizinho/services/group_service.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -23,7 +24,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
     group = BlocProvider.of<GroupBloc>(context).state.activeGroup!;
   }
 
-  List<SkillGauge> skillGauges() {
+  List<SkillGauge> _skillGauges() {
     List<SkillGauge> skillGauges = [];
     for (Skill skill in Skill.values) {
       skillGauges.add(SkillGauge(
@@ -40,130 +41,118 @@ class _SettingsScreenState extends State<SettingsScreen> {
     return skillGauges;
   }
 
-  void removeGroup() {
-    setState(() {
-      GroupService.removeGroup(GroupService.activeGroup());
-      Navigator.pushReplacementNamed(context, "/");
-    });
-  }
-
-  void saveChanges(Group group) {
-    if (group.name == null || group.name!.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          backgroundColor: Colors.red[700],
-          content: const Text(
-            "O nome do grupo não pode estar vazio!",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-      );
-      return;
-    }
-    GroupService.updateGroup(group);
-    Navigator.pushReplacementNamed(context, "/main_group");
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFCDE8DE),
-        elevation: 0,
-        foregroundColor: Colors.black,
-        actions: [
-          PopupMenuButton(
-            itemBuilder: (context) {
-              return [
+    return BlocConsumer<GroupBloc, GroupState>(
+      listener: (context, state) => {
+        if (state is GroupDeletedState)
+          {
+            Navigator.pushReplacementNamed(context, "/"),
+          }
+        else if (state is GroupUpdatedState)
+          {
+            Navigator.pushReplacementNamed(context, "/main_group"),
+          }
+      },
+      builder: (context, state) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFCDE8DE),
+          elevation: 0,
+          foregroundColor: Colors.black,
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (context) => [
                 const PopupMenuItem(
                   value: 0,
                   child: Text("Deletar Grupo"),
                 ),
-              ];
-            },
-            onSelected: (value) {
-              if (value == 0) {
-                removeGroup();
-              }
-            },
-          ),
-        ],
-      ),
-      drawer: const CustomDrawer(),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextField(
-                    controller: TextEditingController(text: group!.name),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Nome do Grupo',
-                      labelStyle: TextStyle(
-                        fontFamily: "poller_one",
-                        color: Colors.grey,
+              ],
+              onSelected: (value) {
+                if (value == 0) {
+                  BlocProvider.of<GroupBloc>(context)
+                      .add(GroupDeleteEvent(state.activeGroup!));
+                }
+              },
+            ),
+          ],
+        ),
+        drawer: const CustomDrawer(),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextField(
+                      controller: TextEditingController(text: group!.name),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Nome do Grupo',
+                        labelStyle: TextStyle(
+                          fontFamily: "poller_one",
+                          color: Colors.grey,
+                        ),
                       ),
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontFamily: "poller_one",
+                      ),
+                      onChanged: (text) {
+                        group!.name = text;
+                      },
                     ),
-                    style: const TextStyle(
-                      color: Colors.grey,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "BALANCEAMENTO POSICIONAL:",
+                          style:
+                              TextStyle(fontSize: 12, fontFamily: "poller_one"),
+                        ),
+                        Checkbox(
+                            value: group!.usePositionalBalancing,
+                            onChanged: (value) {
+                              setState(() {
+                                group!.usePositionalBalancing = value!;
+                              });
+                            })
+                      ],
+                    ),
+                    const Text(
+                      "Peso Por Skill",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "poller_one",
+                          fontWeight: FontWeight.bold),
+                    ),
+                    ..._skillGauges(),
+                  ],
+                ),
+                TextButton(
+                  onPressed: () => BlocProvider.of<GroupBloc>(context)
+                      .add(GroupUpdateEvent(group!)),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: const EdgeInsets.all(20),
+                    elevation: 6,
+                  ),
+                  child: const Text(
+                    "Salvar",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
                       fontFamily: "poller_one",
                     ),
-                    onChanged: (text) {
-                      group!.name = text;
-                    },
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "BALANCEAMENTO POSICIONAL:",
-                        style:
-                            TextStyle(fontSize: 12, fontFamily: "poller_one"),
-                      ),
-                      Checkbox(
-                          value: group!.usePositionalBalancing,
-                          onChanged: (value) {
-                            setState(() {
-                              group!.usePositionalBalancing = value!;
-                            });
-                          })
-                    ],
-                  ),
-                  const Text(
-                    "Peso Por Skill",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "poller_one",
-                        fontWeight: FontWeight.bold),
-                  ),
-                  ...skillGauges(),
-                ],
-              ),
-              TextButton(
-                onPressed: () => saveChanges(group!),
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  padding: const EdgeInsets.all(20),
-                  elevation: 6,
-                ),
-                child: const Text(
-                  "Salvar",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontFamily: "poller_one",
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
