c7306d6d066d668a8237ee18b2b7c29e39170ab8
Felipe Araujo <fbma@cin.ufpe.br>
2023-11-20 11:14:12 -0300

    feat(PlayersBloc): Create PlayersBloc events

---
+++ lib/bloc/player/players_event.dart
@@ -0,0 +1,46 @@
+import 'package:voleizinho/model/player.dart';
+
+abstract class PlayersEvent {}
+
+class PlayersLoadEvent extends PlayersEvent {
+  final int groupId;
+
+  PlayersLoadEvent(this.groupId);
+}
+
+class CreatePlayerEvent extends PlayersEvent {
+  final Player player;
+
+  CreatePlayerEvent(this.player);
+}
+
+class EditPlayerEvent extends PlayersEvent {
+  final Player player;
+  final int index;
+
+  EditPlayerEvent(this.player, this.index);
+}
+
+class DeletePlayerEvent extends PlayersEvent {
+  final Player player;
+
+  DeletePlayerEvent(this.player);
+}
+
+class ImportPlayersEvent extends PlayersEvent {
+  final int groupId;
+
+  ImportPlayersEvent(this.groupId);
+}
+
+class ExportPlayersEvent extends PlayersEvent {
+  final int groupId;
+
+  ExportPlayersEvent(this.groupId);
+}
+
+class DeleteAllPlayersEvent extends PlayersEvent {
+  final int groupId;
+
+  DeleteAllPlayersEvent(this.groupId);
+}
