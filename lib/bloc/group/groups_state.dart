2b6f1d151028438ff2c098547fa3077321381321
Felipe Araujo <fbma@cin.ufpe.br>
2023-11-21 19:32:03 -0300

    feat(GroupBloc): Create Groups State

---
+++ lib/bloc/group/groups_state.dart
@@ -0,0 +1,34 @@
+import 'package:voleizinho/model/group.dart';
+
+enum GroupsStatus { initial, loading, loaded, created, edited, deleted, error }
+
+class GroupsState {
+  final GroupsStatus status;
+  final Group? activeGroup;
+  final List<Group> groups;
+  final String? errorMessage;
+
+  GroupsState({
+    required this.status,
+    this.activeGroup,
+    required this.groups,
+    this.errorMessage,
+  });
+
+  factory GroupsState.initial() =>
+      GroupsState(status: GroupsStatus.initial, groups: []);
+
+  GroupsState copyWith({
+    GroupsStatus? status,
+    Group? activeGroup,
+    List<Group>? groups,
+    String? errorMessage,
+  }) {
+    return GroupsState(
+      status: status ?? this.status,
+      activeGroup: activeGroup ?? this.activeGroup,
+      groups: groups ?? this.groups,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+}
